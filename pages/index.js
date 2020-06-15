b387a7314ab76d8224773b609c2ab543f4f8d01d
slowrunner7 <awperez77@gmail.com>
2020-06-15 10:58:46 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => (
+    <div>
+      <p>Sample app using React and Next.js</p>
+      <p>Get bent Hippy</p>
+    </div>
+  );
+
+  // what does this line mean?
+  export default Index;
+
